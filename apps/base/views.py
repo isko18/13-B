9dd141f107689f6ab1bca6afd4fb13ccc651dd4a
isko18 <toksonbaevislam2004@gmail.com>
2024-01-21 19:42:01 +0600

    d

---
+++ apps/base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def index(request):
+    title = "Geeks"
+    settings = Settings.objects.latest('id')
+    return render(request, 'index.html', locals())
+
+def contact(request):
+    settings = Settings.objects.latest('id')
+    return render(request, 'contact.html', locals())
+
+def blog(request):
+    settings = Settings.objects.latest('id')
+    return render(request, 'blog.html', locals())
